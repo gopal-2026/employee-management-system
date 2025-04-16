55358dfb38ba29d8d3e57f0148f2d0002fe932a6
gopal-2026 <rathoregopalsingh2023@gmail.com>
2025-04-17 02:07:49 +0530

    Ready for Vercel deployment

--- employee-management-system/src/context/EmployeeContext.js
+++ employee-management-system/src/context/EmployeeContext.js
@@ -1,7 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/employees';
+const API_URL = "https://employee-backend-mdk2.onrender.com/api/employees";
 
 export const EmployeeContext = createContext();
 
